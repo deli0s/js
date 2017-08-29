c802efea5556e5bda7c3878e14c6c1d8d2139f12
deli0s <info.delios@gmail.com>
2017-08-29 22:19:23 +0200

    Update yt.js

--- yt.js
+++ yt.js
@@ -7,9 +7,9 @@ function edit(){
 				var video=videos[i_video];
 				if (existeix(video)){
 					var watched=video.getElementsByClassName("ytd-thumbnail-overlay-resume-playback-renderer")[0];
-					if (existeix(watched) || t_(video,"Late Motiv","Broncano,Romero,ignatius") || t_(video,"TERRAT","NADIE SABE NADA") || t_(video,"manga","Titan") || t_(video,"Oh! My LOL","Moderna ") || t_(video,"Jimmy Fallon","Hashtags") || t_(video,"WIRED","Answer the Web") || t_(video,"Pazos64","Cuidao Ahí")){
+					if (existeix(watched) || t_(video,"Late Motiv","Broncano,Berto,Romero,ignatius") || t_(video,"TERRAT","NADIE SABE NADA") || t_(video,"manga","Titan") || t_(video,"Oh! My LOL","Moderna ") || t_(video,"Jimmy Fallon","Hashtags") || t_(video,"WIRED","Answer the Web") || t_(video,"Pazos64","Cuidao Ahí")){
 						deleteByClass("yt-shelf-grid-item",i_video);
-						i_video--;
+						//i_video--;
 					}
 				//console.log(i_video);
 				}
